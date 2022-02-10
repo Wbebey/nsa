b74d47233cdddbf88f0f0ce178aa9789ac9fc480
Wbebey <wilfried@armis.tech>
2022-02-10 11:55:48 +0100

    feature: gitlab-ci

--- gitlab.sh
+++ gitlab.sh
@@ -2,16 +2,28 @@
 # Get the registration token from:
 # http://localhost/root/${project}/settings/ci_cd
 
-registration_token="KrscBuZyDCKxDrbEC1yg"
+registration_token="_qFcQLKRAKR3GUrxbWzK"
 
 docker exec -it gitlab-runner1 \
     gitlab-runner register \
-    --non-interactive \
-    --registration-token ${registration_token} \
-    --locked=false \
-    --description docker-stable \
-    --url http://gitlab-web \
-    --executor docker \
-    --docker-image docker:stable \
-    --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" \
-    --docker-network-mode gitlab-network
+        --non-interactive \
+        --registration-token ${registration_token} \
+        --locked=false \
+        --description docker-stable \
+        --url http://gitlab-web \
+        --executor docker \
+        --docker-image etiennec27/laravelphp:latest \
+        --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" \
+        --docker-network-mode gitlab-network
+
+docker exec -it gitlab-runner2 \
+    gitlab-runner register \
+        --non-interactive \
+        --registration-token ${registration_token} \
+        --locked=false \
+        --description docker-stable \
+        --url http://gitlab-web \
+        --executor docker \
+        --docker-image etiennec27/laravelphp:latest \
+        --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" \
+        --docker-network-mode gitlab-network
